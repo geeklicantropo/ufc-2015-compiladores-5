4a38020ec94b8e685e04c334ff8a984fc9c01913
arturmesquitab <arturmesquitab@gmail.com>
2015-04-12 22:31:55 -0300

    ajuste ações semânticas

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 
 import reader.ReadFile;
 
+import syntaxtree.*;
+import visitor.*;
+
 public class Main {
    
    public static void main(String [] args) throws IOException {
